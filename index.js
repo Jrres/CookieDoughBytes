d0caf082e1952dfa937ba5259010bd5e5517ce67
Jack <80206848+Jrres@users.noreply.github.com>
2021-04-04 15:24:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,365 @@
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3,
+  NEWLEVEL: 4
+};
+class Paddle {
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
+
+    this.width = 150;
+    this.height = 20;
+
+    this.maxSpeed = 7;
+    this.speed = 0;
+
+    this.position = {
+      x: game.gameWidth / 2 - this.width / 2,
+      y: game.gameHeight - this.height - 10
+    };
+  }
+
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
+
+  stop() {
+    this.speed = 0;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "#0ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    this.position.x += this.speed;
+
+    if (this.position.x < 0) this.position.x = 0;
+
+    if (this.position.x + this.width > this.gameWidth)
+      this.position.x = this.gameWidth - this.width;
+  }
+}
+//bat
+  class Bat {
+  constructor(game) {
+    this.image = document.getElementById("img_bat");
+
+    this.gameWidth = game.gameWidth;
+    this.gameHeight = game.gameHeight;
+
+    this.game = game;
+    this.size = 60;
+    this.reset();
+  }
+
+  reset() {
+    this.position = { x: 10, y: 400 };
+    this.speed = { x: 4, y: -2 };
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.size,
+      this.size
+    );
+  }
+
+  update(deltaTime) {
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+
+    // wall on left or right
+    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+
+    // wall on top
+    if (this.position.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
+
+    // bottom of game
+    if (this.position.y + this.size > this.gameHeight) {
+      this.game.lives--;
+      this.reset();
+    }
+
+    if (detectCollision(this, this.game.paddle)) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
+    }
+  }
+}
+function detectCollision(bat, gameObject) {
+  let bottomOfbat = bat.position.y + bat.size;
+  let topOfbat = bat.position.y;
+
+  let topOfObject = gameObject.position.y;
+  let leftSideOfObject = gameObject.position.x;
+  let rightSideOfObject = gameObject.position.x + gameObject.width;
+  let bottomOfObject = gameObject.position.y + gameObject.height;
+
+  if (
+    bottomOfbat >= topOfObject &&
+    topOfbat <= bottomOfObject &&
+    bat.position.x >= leftSideOfObject &&
+    bat.position.x + bat.size <= rightSideOfObject
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//paddle
+
+
+//levels
+function buildLevel(game, level) {
+  let bricks = [];
+
+  level.forEach((row, rowIndex) => {
+    row.forEach((brick, brickIndex) => {
+      if (brick === 1) {
+        let position = {
+          x: 80 * brickIndex,
+          y: 75 + 24 * rowIndex
+        };
+        bricks.push(new Brick(game, position));
+      }
+    });
+  });
+
+  return bricks;
+}
+
+const level1 = [
+  // [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+];
+
+ const level2 = [
+  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+class Brick {
+  constructor(game, position) {
+    this.image = document.getElementById("img_purpstone");
+
+    this.game = game;
+
+    this.position = position;
+    this.width = 80;
+    this.height = 24;
+
+    this.markedForDeletion = false;
+  }
+
+  update() {
+    if (detectCollision(this.game.bat, this)) {
+      this.game.bat.speed.y = -this.game.bat.speed.y;
+
+      this.markedForDeletion = true;
+    }
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+class InputHandler {
+  constructor(paddle, game) {
+    document.addEventListener("keydown", event => {
+      switch (event.keyCode) {
+        case 37:
+          paddle.moveLeft();
+          break;
+
+        case 39:
+          paddle.moveRight();
+          break;
+
+        case 27:
+          game.togglePause();
+          break;
+
+        case 32:
+          game.start();
+          break;
+      }
+    });
+
+    document.addEventListener("keyup", event => {
+      switch (event.keyCode) {
+        case 37:
+          if (paddle.speed < 0) paddle.stop();
+          break;
+
+        case 39:
+          if (paddle.speed > 0) paddle.stop();
+          break;
+      }
+    });
+  }
+}
+class Game {
+
+  constructor(gameWidth, gameHeight, bricksPerRow) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.gamestate = GAMESTATE.MENU;
+    this.bat = new Bat(this);
+    this.paddle = new Paddle(this);
+    this.gameObjects = [];
+    this.bricks = [];
+    this.lives = 3;
+
+    this.levels = [level1, level2];
+    this.currentLevel = 0;
+
+    new InputHandler(this.paddle, this);
+  }
+
+  start() {
+    if (
+      this.gamestate !== GAMESTATE.MENU &&
+      this.gamestate !== GAMESTATE.NEWLEVEL
+    )
+      return;
+
+    this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+    this.bat.reset();
+    this.gameObjects = [this.bat, this.paddle];
+
+    this.gamestate = GAMESTATE.RUNNING;
+  }
+
+  update(deltaTime) {
+    if (this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER;
+
+    if (
+      this.gamestate === GAMESTATE.PAUSED ||
+      this.gamestate === GAMESTATE.MENU ||
+      this.gamestate === GAMESTATE.GAMEOVER
+    )
+      return;
+
+    if (this.bricks.length === 0) {
+      this.currentLevel++;
+      this.gamestate = GAMESTATE.NEWLEVEL;
+      this.start();
+    }
+
+    [...this.gameObjects, ...this.bricks].forEach(object =>
+      object.update(deltaTime)
+    );
+
+    this.bricks = this.bricks.filter(brick => !brick.markedForDeletion);
+  }
+
+  draw(ctx) {
+    [...this.gameObjects, ...this.bricks].forEach(object => object.draw(ctx));
+
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,0.5)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+    }
+
+    if (this.gamestate === GAMESTATE.MENU) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText(
+        "Press SPACEBAR To Start",
+        this.gameWidth / 2,
+        this.gameHeight / 2
+      );
+    }
+    if (this.gamestate === GAMESTATE.GAMEOVER) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2);
+    }
+  }
+
+  togglePause() {
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
+  }
+}
+
+
 var canvas = document.getElementById("gameScreen");
-var ctx = canvas.getContext("2d");
-ctx.lineWidth = 10;
+let ctx = canvas.getContext("2d");
+ 
 
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
+    const GAME_WIDTH = 800;
+    const GAME_HEIGHT = 600;
+    ctx.fillRect(0,0,100,100);
+    ctx.fillStyle="blue";
+    let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+  
+ function gameLoop(timestamp) {
+  let lastTime = 0;
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
 
-// Door
-ctx.fillRect(130, 190, 40, 60);
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
-// Roof
-ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
+  game.update(deltaTime);
+  game.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+requestAnimationFrame(gameLoop);
+
+//main
+
+
+//collsion
+
+
+
+
+
